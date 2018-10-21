da0e6637c03e041ab159432a301097fdbc11fdb3
John Newman <newman719@gmail.com>
2018-10-21 18:46:49 -0400

    Changing the stream saver to read up to the latest frame. read_from_stream is now an instance method.

--- streamer/StreamSaver.py
+++ streamer/StreamSaver.py
@@ -3,8 +3,11 @@ import time
 import picamera
 import logging
 
-MAX_READ_BYTES = 1024*1024*5  # 5 MB
+MAX_READ_BYTES = int(1024*1024*2.5)  # 2.5 MB
 SLEEP_TIME = 0.5  # Seconds
+
+global_index = 0
+index_lock = Lock()
 
 
 class StreamSaver(Thread):
@@ -40,50 +43,66 @@ class StreamSaver(Thread):
         return should_stop
 
     def stop(self):
-        """Used to stop uploading. When set, the last array of bytes will be
+        """
+        Used to stop uploading. When set, the last array of bytes will be
         sent to the ``byte_writer`` with the ``close`` flag.  Finally, the
-        thread will stop."""
-
+        thread will stop.
+        """
         self.__lock.acquire()
         self.__stop = True
         self.__lock.release()
 
-    def run(self):
-        """Loops over the stream, reads ``MAX_READ_BYTES`` chunks of bytes, and
-        sends these to the ``byte_writer``."""
+    def read_from_stream(self, position, length=MAX_READ_BYTES):
+        """
+        :param position: The position to start reading.
+        :param length: The length of bytes to read.
+        :return: a tuple of the bytes read and the position where reading
+        stopped.
+        """
+        if position is None:
+            position = 0
+        original_position = self.__stream.tell()
+        self.__stream.seek(position)
+        read_bytes = self.__stream.read(length)
+        self.__stream.seek(original_position)  # Restore where the stream was
+        if read_bytes is None:
+            read_bytes = ''
+        return read_bytes, position + len(read_bytes)
 
+    def run(self):
+        """
+        Loops over the stream and reads bytes in either ``MAX_READ_BYTES``
+        sized chunks (if using a normal stream) or reads from the previous
+        frame position to the newest frame (if using a camera stream.).  All
+         bytes are sent to the ``byte_writer``.
+         """
+
+        global global_index
         logger = logging.getLogger(__name__ + '.' + self.name)
         try:
-            # Returns bytes read at the position and the updated position
-            def read_from_stream(stream, position):
-                if position == -1:
-                    position = 0
-                    logger.debug('Using stream start 0.')
+            index_lock.acquire()
+            min_frame_index = global_index  # Start at a minimum of where the last streamer left off
+            index_lock.release()
 
-                __restore_position = stream.tell()
-                stream.seek(position)
-                __read_bytes = stream.read(MAX_READ_BYTES)  # Read from where we left off
-                if __read_bytes is None:
-                    __read_bytes = ''
-                __stream_pos = position + len(__read_bytes)
-                stream.seek(__restore_position)  # Restore where the stream was
-                return __read_bytes, __stream_pos
-
-            stream_pos = -1
+            stream_pos = None
             stopped = False
             total_bytes = 0
             while not stopped:
                 if isinstance(self.__stream, picamera.PiCameraCircularIO):
                     with self.__stream.lock:  # Live camera stream needs to be locked while read
-                        if stream_pos == -1:
+                        if stream_pos is None:
                             for frame in self.__stream.frames:
                                 if frame.frame_type == picamera.PiVideoFrameType.sps_header:
-                                    stream_pos = frame.position
-                                    logger.debug('Found first sps header at position %d' % frame.position)
-                                    break
-                        read_bytes, stream_pos = read_from_stream(self.__stream, stream_pos)
+                                    if stream_pos is None or frame.index <= min_frame_index:  # Find latest SPS header.
+                                        stream_pos = frame.position
+                            logger.debug('Newest SPS header is %d at position %d.' % (frame.index, stream_pos))
+                        last_frame = next(reversed(self.__stream.frames))  # Read to the last frame.
+                        min_frame_index = last_frame.index
+                        end_pos = last_frame.position
+                        logger.debug('Newest frame is %d at position %d.' % (last_frame.index, last_frame.position))
+                        read_bytes, stream_pos = self.read_from_stream(stream_pos, length=end_pos-stream_pos)
                 else:
-                    read_bytes, stream_pos = read_from_stream(self.__stream, stream_pos)
+                    read_bytes, stream_pos = self.read_from_stream(stream_pos)
 
                 logger.debug('Read %d bytes.' % len(read_bytes)) if len(read_bytes) > 0 else None
                 total_bytes += len(read_bytes)
@@ -92,8 +111,14 @@ class StreamSaver(Thread):
 
                 if len(read_bytes) == 0:
                     time.sleep(SLEEP_TIME)  # Wait for more data in the stream
-            logger.info('Processed %d bytes' % total_bytes)
+            logger.info('Processed %d total bytes.' % total_bytes)
+
+            index_lock.acquire()  # Update the min frame index for the next streamer.
+            if min_frame_index > global_index:
+                global_index = min_frame_index
+                logger.debug('Updated the minimum frame index to %d.' % global_index)
+            index_lock.release()
+
         except Exception as e:
-            # Try to close if we have an exception
-            self.__byte_writer.append_bytes('', close=True)
+            self.__byte_writer.append_bytes('', close=True)  # Try to close if we have an exception
             logger.exception('An exception occurred: %s' % e.message)
