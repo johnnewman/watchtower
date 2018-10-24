11f7e4861de94756eafea4e8805d3f83663b8ed1
John Newman <newman719@gmail.com>
2018-10-23 20:15:07 -0400

    Now using a json config file instead of supplied arguments. Adding extra json fields for date formats, rotation, and framerate. Moving the location of the init_camera function.

--- PiSecCam.py
+++ PiSecCam.py
@@ -16,37 +16,49 @@ WAIT_TIME = 0.2
 INITIALIZATION_TIME = 3  # In Seconds
 
 
-def init_camera():
-    camera = picamera.PiCamera(resolution=(supplied_args["video_width"], supplied_args["video_height"]),
-                               framerate=15)
-    camera.rotation = 180
-    camera.annotate_background = picamera.Color('black')
-    camera.annotate_text_size = 12
-    return camera
+def init_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c",
+                        "--config-file",
+                        type=str,
+                        default='config/camera_config.json',
+                        help='location of the config json file.')
+    supplied_args = vars(parser.parse_args())
+    with open(supplied_args['config_file'], 'r') as config_file:
+        return json.load(config_file)
 
 
 def init_logging():
     log_dir = 'logs/'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    with open('config/log_config.json', 'r') as config_file:
-        logging.config.dictConfig(json.load(config_file))
+    with open('config/log_config.json', 'r') as log_config_file:
+        logging.config.dictConfig(json.load(log_config_file))
     return logging.getLogger(__name__)
 
 
 def init_command_receiver():
-    if supplied_args['command_port'] is not None:
+    if config['command_port'] is not None:
         remote.CommandReceiver(get_running_callback=get_running,
                                set_running_callback=set_running,
-                               port=supplied_args['command_port']).start()
+                               port=config['command_port']).start()
     return Lock(), True
+
+
+def init_camera():
+    camera = picamera.PiCamera(resolution=(tuple(config['video_size'])),
+                               framerate=config['framerate'])
+    camera.rotation = config['rotation']
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_text_size = 12
+    return camera
 
 
 def wait(camera):
     """Single point where we record camera data. This also updates the
     annotation on the feed."""
 
-    camera.annotate_text = supplied_args["cam_name"] + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    camera.annotate_text = config['cam_name'] + dt.datetime.now().strftime(config['overlay_date_format'])
     camera.wait_recording(WAIT_TIME)
 
 
@@ -59,9 +71,9 @@ def save_stream(stream, path, debug_name, stop_when_empty=False):
                                       name=debug_name+'.loc',
                                       stop_when_empty=stop_when_empty)]
 
-    if supplied_args["dropbox_token"] is not None:
+    if config['dropbox_token'] is not None:
         dropbox_writer = writer.DropboxWriter(full_path='/' + path,
-                                              dropbox_token=supplied_args["dropbox_token"])
+                                              dropbox_token=config['dropbox_token'])
         streamers.append(streamer.StreamSaver(stream=stream,
                                               byte_writer=dropbox_writer,
                                               name=debug_name+'.dbx',
@@ -90,9 +102,8 @@ def get_running():
 def main():
     camera = init_camera()
     with camera:
-        min_capture_time = supplied_args['min_capture_time']
-        motion_detector = motion.MotionDetector(camera, supplied_args['min_delta'], supplied_args['min_area'])
-        stream = picamera.PiCameraCircularIO(camera, seconds=min_capture_time)
+        motion_detector = motion.MotionDetector(camera, config['min_delta'], config['min_trigger_area'])
+        stream = picamera.PiCameraCircularIO(camera, seconds=config['min_rec_time_after_trigger'])
         camera.start_recording(stream, format='h264')
         logger.info('Initialized.')
 
@@ -113,9 +124,9 @@ def main():
                     logger.info('Motion detected!')
 
                     event_date = dt.datetime.now()
-                    day_str = event_date.strftime('%Y-%m-%d')
-                    time_str = event_date.strftime('%H.%M.%S')
-                    full_dir = supplied_args["cam_name"] + '/' + day_str + '/' + time_str
+                    day_str = event_date.strftime(config['day_dir_date_format'])
+                    time_str = event_date.strftime(config['time_dir_date_format'])
+                    full_dir = config['cam_name'] + '/' + day_str + '/' + time_str
 
                     save_stream(io.BytesIO(motion_frame_bytes),
                                 path=full_dir + '/motion0.jpg',
@@ -126,14 +137,14 @@ def main():
                                                   debug_name=time_str+'.vid')
 
                     # Capture a minimum amount of video after motion
-                    while (dt.datetime.now() - event_date).seconds < min_capture_time:
+                    while (dt.datetime.now() - event_date).seconds < config['min_rec_time_after_trigger']:
                         wait(camera)
 
                     # Wait for motion to stop
                     last_motion_check = time.time()
                     motion_count = 0
                     while motion_detected:
-                        if time.time() - last_motion_check >= min_capture_time:
+                        if time.time() - last_motion_check >= config['min_rec_time_after_trigger']:
                             motion_detected, motion_frame_bytes = motion_detector.detect()
                             if motion_detected:
                                 logger.debug('More motion detected!')
@@ -157,17 +168,7 @@ def main():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--min-area", type=int, default=.005, help="minimum percent of video for motion")
-    parser.add_argument("-W", "--video-width", type=int, default=1280, help="video capture width")
-    parser.add_argument("-H", "--video-height", type=int, default=720, help="video capture height")
-    parser.add_argument("-t", "--min-delta", type=int, default=50, help="minimum delta gray value to threshold")
-    parser.add_argument("-c", "--cam-name", type=str, default='PySecCam', help="name of the camera")
-    parser.add_argument("-m", "--min-capture-time", type=int, default=8, help="minimum time to capture motion")
-    parser.add_argument("-T", "--dropbox-token", type=str, help="token for Dropbox")
-    parser.add_argument("-p", "--command-port", type=int, help="port to listen for commands")
-    supplied_args = vars(parser.parse_args())
-
+    config = init_config()
     logger = init_logging()
     status_lock, running = init_command_receiver()
     main()
