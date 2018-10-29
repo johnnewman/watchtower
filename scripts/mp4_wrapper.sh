7bb1ca9b0eab09adade6fb196a313eb4f0e52c50
John Newman <newman719@gmail.com>
2018-10-28 21:55:17 -0400

    Removing spaces from the output directories to work with MP4Box.

--- scripts/mp4_wrapper.sh
+++ scripts/mp4_wrapper.sh
@@ -8,15 +8,18 @@
 # Searches the input directory for .h264 files and combines the ones with
 # shared directories into one file in the output directory. All the blended
 # .h264 files in the output directory are wrapped in an mp4 format. The output
-# directory mirrors the structure of the input directory.
+# directory mirrors the structure of the input directory except spaces dir
+# names are removed.
 
 fps=20
 
 # Append all the .h264 files into one
 find "$1" -name "*.h264" | sort | while read file; do
-    file_dir=`dirname ${file//"$1"/}`
-    new_file="$2$file_dir"/blended_video.h264
-    mkdir -p `dirname "$new_file"`
+    short_path=${file//"$1"/}
+    short_dir=`dirname "$short_path"`
+    short_dir=${short_dir//[[:blank:]]/} # MP4Box does not like spaces
+    new_file="$2$short_dir"/blended_video.h264
+    mkdir -p "`dirname "$new_file"`"
     cat "$file" >> "$new_file"
 done
 
