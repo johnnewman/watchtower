7f34723ea13b620e6e136b3d5fdc30cb36602266
John Newman <newman719@gmail.com>
2018-11-02 20:03:19 -0400

    Updates to the config file structure. Adding additional parameters to the command server.

--- PiSecCam.py
+++ PiSecCam.py
@@ -39,18 +39,19 @@ def init_logging():
 
 
 def init_command_server():
-    if command_port is not None:
-        remote.CommandServer(get_camera_callback=get_camera,
-                             get_running_callback=get_running,
-                             set_running_callback=set_running,
-                             port=command_port).start()
-    return Lock(), False
+    remote.CommandServer(get_camera_callback=get_camera,
+                         get_running_callback=get_running,
+                         set_running_callback=set_running,
+                         port=config['server']['server_port'],
+                         certfile=config['server']['certfile_path'],
+                         keyfile=config['server']['keyfile_path'],
+                         mjpeg_rate=config['server']['mjpeg_rate_sec']).start()
 
 
 def init_camera():
-    camera = picamera.PiCamera(resolution=(video_size),
-                               framerate=framerate)
-    camera.rotation = rotation
+    camera = picamera.PiCamera(resolution=tuple(config['video_size']),
+                               framerate=config['framerate'])
+    camera.rotation = config['rotation']
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text_size = 12
     return camera
@@ -60,7 +61,7 @@ def wait(camera):
     """Single point where we record camera data. This also updates the
     annotation on the feed."""
 
-    camera.annotate_text = cam_name + dt.datetime.now().strftime(overlay_date_format)
+    camera.annotate_text = cam_name + dt.datetime.now().strftime(config['formats']['overlay_date_format'])
     camera.wait_recording(WAIT_TIME)
 
 
@@ -68,7 +69,7 @@ def save_stream(stream, path, debug_name, stop_when_empty=False):
     """Saves the ``stream`` to disk and optionally Dropbox, if a Dropbox token
      was supplied as a parameter to the program."""
 
-    stream_start_time = max(0, int(time.time() - start_time - rec_time_before_trigger))
+    stream_start_time = max(0, int(time.time() - start_time - rec_sec_before_trigger))
 
     def create_cam_stream(_debug_name, byte_writer):
         return streamer.CamStreamSaver(stream=stream,
@@ -86,7 +87,7 @@ def save_stream(stream, path, debug_name, stop_when_empty=False):
     def create_dropbox_writer(_path):
         return writer.DropboxWriter(full_path=_path,
                                     dropbox_token=dropbox_token,
-                                    file_chunk_size=dropbox_file_chunk_megs)
+                                    file_chunk_size=dropbox_chunk_size)
 
     streamers = []
     if isinstance(stream, picamera.PiCameraCircularIO):
@@ -126,8 +127,16 @@ def get_camera():
 
 def main():
     with camera:
-        motion_detector = motion.MotionDetector(camera, min_delta, min_trigger_area)
-        stream = picamera.PiCameraCircularIO(camera, seconds=rec_time_before_trigger)
+        day_dir_format = config['formats']['day_directory_format']
+        time_dir_format = config['formats']['time_directory_format']
+        max_event_time = config['motion']['max_event_time']
+        rec_sec_after_trigger = config['motion']['rec_sec_after']
+
+        area = config['motion']['min_trigger_area']
+        delta = config['motion']['min_pixel_delta_trigger']
+        motion_detector = motion.MotionDetector(camera, delta, area)
+
+        stream = picamera.PiCameraCircularIO(camera, seconds=config['motion']['rec_sec_before'])
         camera.start_recording(stream, format='h264')
         logger.info('Initialized.')
 
@@ -148,13 +157,13 @@ def main():
                     logger.info('Motion detected!')
                     event_time = time.time()
                     event_date = dt.datetime.now()
-                    day_str = event_date.strftime(day_dir_date_format)
-                    time_str = event_date.strftime(time_dir_date_format)
+                    day_str = event_date.strftime(day_dir_format)
+                    time_str = event_date.strftime(time_dir_format)
                     full_dir = cam_name + '/' + day_str + '/' + time_str
 
                     save_stream(io.BytesIO(motion_frame_bytes),
-                                path=full_dir + '/motion0.jpg',
-                                debug_name=time_str + '.jpg0',
+                                path=full_dir + '/motion.jpg',
+                                debug_name=time_str + '.jpg',
                                 stop_when_empty=True)
                     video_streamers = save_stream(stream,
                                                   path=full_dir+'/video.h264',
@@ -162,8 +171,8 @@ def main():
 
                     # Wait for motion to stop
                     last_motion_trigger = time.time()
-                    while get_running() and time.time() - last_motion_trigger <= rec_time_after_trigger and \
-                            time.time() - event_time <= max_trigger_time:
+                    while get_running() and time.time() - last_motion_trigger <= rec_sec_after_trigger and \
+                            time.time() - event_time <= max_event_time:
                         more_motion, motion_frame_bytes = motion_detector.detect()
                         if more_motion:
                             logger.debug('More motion detected!')
@@ -183,28 +192,19 @@ def main():
 
 
 if __name__ == '__main__':
-    config = init_config()
-
-    cam_name = config['cam_name']
-    command_port = config['command_port']
-    day_dir_date_format = config['day_dir_date_format']
-    dropbox_token = config['dropbox_token']
-    framerate = config['framerate']
-    max_trigger_time = config['max_trigger_time']
-    dropbox_file_chunk_megs = config['dropbox_file_chunk_megs']
-    if dropbox_file_chunk_megs is not None:
-        dropbox_file_chunk_megs *= 1024*1024
-    min_delta = config['min_delta']
-    min_trigger_area = config['min_trigger_area']
-    overlay_date_format = config['overlay_date_format']
-    rec_time_after_trigger = config['rec_time_after_trigger']
-    rec_time_before_trigger = config['rec_time_before_trigger']
-    rotation = config['rotation']
-    time_dir_date_format = config['time_dir_date_format']
-    video_size = tuple(config['video_size'])
-
     logger = init_logging()
-    status_lock, running = init_command_server()
-    start_time = time.time()
+    config = init_config()
+    cam_name = config['cam_name']
+    dropbox_token = config['dropbox']['token']
+    dropbox_chunk_size = config['dropbox']['file_chunk_megs']
+    if dropbox_chunk_size is not None:
+        dropbox_chunk_size *= 1024 * 1024
+    server_enabled = config['server']['enabled']
+
+    if server_enabled:
+        init_command_server()
+    status_lock = Lock()
     camera = init_camera()
+    start_time = time.time()
+    running = True
     main()
