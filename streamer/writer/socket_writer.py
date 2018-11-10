6fadd04243ff14bb6b7f5473637f102c69727a32
John Newman <newman719@gmail.com>
2018-11-09 20:08:47 -0500

    Adding documentation.

--- streamer/writer/socket_writer.py
+++ streamer/writer/socket_writer.py
@@ -1,17 +1,25 @@
 import socket
 from streamer.writer import byte_writer
 
-
 BOUNDARY = 'FRAME'
 
 
 class SocketWriter(byte_writer.ByteWriter):
+    """
+    A class that writes all bytes to its provided socket.
+    """
 
     def __init__(self, comm_socket):
         super(SocketWriter, self).__init__(None)
         self.__socket = comm_socket
 
     def append_bytes(self, byte_string, close=False):
+        """
+        Writes the byte_string to the socket in its entirety. Any exception is
+        thrown to the caller.
+        :param byte_string: The string to write to the socket.
+        :param close: When True, this will close the socket after writing.
+        """
         total_sent = 0
         while total_sent < len(byte_string) and len(byte_string) > 0:
             sent = self.__socket.send(byte_string[total_sent:])
@@ -24,6 +32,11 @@ class SocketWriter(byte_writer.ByteWriter):
 
 
 class MJPEGSocketWriter(SocketWriter):
+    """
+    A class that appends some HTTP header fluff to the byte string so a browser
+    can interpret the data as an MJPEG stream. This string is sent to the
+    superclass for output.
+    """
 
     def __init__(self, comm_socket):
         super(MJPEGSocketWriter, self).__init__(comm_socket)
