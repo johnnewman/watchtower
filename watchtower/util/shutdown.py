ea3f709bb8e9a90fe38ce44963498284f61c9b50
John Newman <newman719@gmail.com>
2020-09-04 22:15:49 -0400

    Now using TerminableThread in the StreamSaver class. Adding more documentation.

--- watchtower/util/shutdown.py
+++ watchtower/util/shutdown.py
@@ -20,6 +20,12 @@ except ImportError:
 
 
 class TerminableThread(Thread):
+    """
+    This is a threaded class intended to be subclassed. Threads that need to
+    loop in their run() method can check the should_run flag on each loop
+    iteration. This flag will flip to False when Watchtower is shut down. This
+    avoids the program hanging at termination time.
+    """
 
     @property
     def should_run(self):
