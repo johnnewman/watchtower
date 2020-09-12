345eaacee8942fc4c662de56872e93388ff8ce69
John Newman <newman719@gmail.com>
2020-09-12 13:58:23 -0400

    Finishing the delete action. Better list formatting.

--- watchtower/static/scripts/recordings.js
+++ watchtower/static/scripts/recordings.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
+
     var buttons = $('button')
     for (i = 0; i < buttons.length; i++) { 
         button = buttons[i];
         if (button.id.startsWith('recording-download-button-')) {
-            console.log('found button ' + button.id)
             button.addEventListener("click", downloadRecording)
         } else if (button.id.startsWith('recording-delete-button-')) {
-            console.log('found button ' + button.id)
             button.addEventListener("click", deleteRecording)
         }
     }
 
     function downloadRecording(event) {
-        var button = event.target
+        var button = this
         var datetime = button.id.substring('recording-download-button-'.length)
         var elements = datetime.split(" ")
 
@@ -25,27 +24,19 @@ $(document).ready(function(){
     }
       
     function deleteRecording(event) {
-        var button = event.target
+        var button = this
         var datetime = button.id.substring('recording-delete-button-'.length)
         var elements = datetime.split(" ")
-
-        var group = $('#list-group-'+elements[0]+' '+elements[1])
-        console.log(group)
-        group.hide()
-        // document.body.removeChild(group)
-
-        // $.ajax({
-        //     type: 'DELETE',
-        //     url: 'api/recordings/'+elements[0]+'/'+elements[1],
-        //     success: function (result, status, xhr) {
-        //         console.log('Delete successful!')
-        //         var group = $('#list-group-'+elements[0]+' '+elements[1])
-        //         console.log(group)
-        //         document.body.removeChild(group)
-        //     },
-        //     error: function (xhr, status, error) {
-        //         console.log(status + ' ' + error + ' ' + xhr.status + ' ' + xhr.statusText)
-        //     }
-        // });
+        $.ajax({
+            type: 'DELETE',
+            url: 'api/recordings/'+elements[0]+'/'+elements[1],
+            success: function (result, status, xhr) {
+                var query = $.escapeSelector('list-group-'+elements[0]+' '+elements[1])
+                $('#'+query).hide()
+            },
+            error: function (xhr, status, error) {
+                console.log(status + ' ' + error + ' ' + xhr.status + ' ' + xhr.statusText)
+            }
+        });
     }
   })
