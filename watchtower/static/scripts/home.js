31a4ed2f96bd1ef66940a143617428cb4e7706e6
John Newman <newman719@gmail.com>
2020-09-12 18:35:15 -0400

    Adding an MJPEG streaming implementation.

--- watchtower/static/scripts/home.js
+++ watchtower/static/scripts/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function () { 
-    
+
     $('#play-mjpeg').click(function (e) {
-        $('#mjpeg-stream').attr('src', 'mjpeg');
+        loadStream()
     });
 
     $('#pause-mjpeg').click(function (e) {
@@ -46,5 +46,44 @@ $(document).ready(function () {
             $('#status').text('stopped');
         }
     }
+
+    function loadStream() {
+        const xhr = new XMLHttpRequest(),
+            method = "GET",
+            url = "mjpeg?encoding=base64";
+        xhr.open(method, url, true);
+
+        var index = 0
+        var lastContentLength = null
+        var loadedImageForIndex = true
+
+        xhr.onreadystatechange = function () {
+            if (xhr.status === 200) {
+                var contentLength = xhr.responseText.match(/Content-Length: \d+$/gm)
+                if (contentLength !== null) {
+                    if (loadedImageForIndex) {
+                        // Find the next image tag
+                        lastContentLength = contentLength[contentLength.length-1]
+                        index = xhr.responseText.lastIndexOf(lastContentLength)
+                        loadedImageForIndex = false
+                    }
+                    var body = xhr.responseText.substring(index + lastContentLength.length)
+                    var nextFrameIndex = body.indexOf('--FRAME')
+                    if (nextFrameIndex !== -1) {
+                        var imageData = body.substring(0, nextFrameIndex)                        
+                        imageData = imageData.trim()
+                        console.log('Length of frame: ' + imageData.length + ' Provided length: ' + lastContentLength)
+                        $('#mjpeg-stream').attr('src', 'data:image/jpeg;base64, ' + imageData)
+                        loadedImageForIndex = true
+                    }
+                }
+            } else {
+                console.log('MJPEG Stream error: ' + xhr.status + ' ' + xhr.statusText);
+            }
+        };
+        xhr.send();
+    }
+
+     
     
   });
