a33847163d89dbe6929ba390c1d88b3213610f98
John Newman <newman719@gmail.com>
2020-12-29 15:49:00 +0000

    Adding trusted client address to nginx config. Adding video group to .env.

--- install.sh
+++ install.sh
@@ -50,15 +50,16 @@ sudo useradd -M watchtower
 echo "Created watchtower user with uid: `id -u watchtower` gid: `id -g watchtower`"
 
 # Add the new UID and GID to the .env file for Docker.
-sed -i "s,<uid>,`id -u watchtower`, ; s,<gid>,`id -g watchtower`," "$WATCHTOWER_PATH/.env"
+sed -i "s,<s_uid>,`id -u watchtower`, ; s,<s_gid>,`id -g watchtower`," "$WATCHTOWER_PATH/.env"
+# Add the video group's GID to the .env file.
+sed -i "s,<v_gid>,`getent group video | awk -F: '{printf "%d", $3}'`", "$WATCHTOWER_PATH/.env"
 
-# Create instance folder
+# Create instance and recordings folders.
 mkdir -p "$WATCHTOWER_PATH/instance/recordings"
 sudo chgrp video "$WATCHTOWER_PATH/instance"
 sudo chmod 775 "$WATCHTOWER_PATH/instance"
 sudo chgrp video "$WATCHTOWER_PATH/instance/recordings"
 sudo chmod 775 "$WATCHTOWER_PATH/instance/recordings"
-
 echo "Created \"$WATCHTOWER_PATH/instance/recordings\""
 
 # Create the logs directory. The watchtower instance will be started using the video group.
@@ -73,19 +74,22 @@ CRON_JOB=`(crontab -l 2>/dev/null ; echo "$CRON_JOB")`
 echo "$CRON_JOB" | crontab
 echo "Created disk_purge cron job."
 
-
-echo -e "\n\nInstallation finished! The camera is configured to record to disk at \"$WATCHTOWER_PATH/instance/recordings\".\n\
-# Final steps to take: \n\
-# 1) Required: Enable camera access via 'sudo raspiconfig'\n\
-# 2) Optional: To use a microcontroller, enable serial access via 'sudo raspiconfig'\n\
-# 3) Optional: To use the HTTP API, upload SSL certificates to \"$WATCHTOWER_PATH/nginx/certs\"\n\
-#              You will need:
-                  - (1) a public SSL cert and (2) the corresponding private key. Used for encrypting traffic
-                  - (3) a certificate authority cert for validating clients
-               These 3 files must match the names defined in \"$WATCHTOWER_PATH/.env\":\n\
-                  \"SSL_CERT=wt.crt\"\n\
-                  \"SSL_CERT_KEY=wt.key\"\n\
-                  \"SSL_CLIENT_CERT=ca.crt\"\n\
-# 4) Optional: Only allow HTTP access from trusted sources by editing /etc/nginx/sites-available/watchtower\n\
-# 5) Optional: Configure the main reverse proxy with an upstream location to this machine. See $WATCHTOWER_PATH/ancillary/nginx/reverse_proxy\n\
-# 6) Optional: Configure $WATCHTOWER_PATH/instance/watchtower_config.json with Dropbox and microcontroller support."
+echo -e "\n\nInstallation finished! The camera is configured to record to disk at \"$WATCHTOWER_PATH/instance/recordings\".\n\n\
+Final steps to take:\n\
+1) REQUIRED: Enable camera access via 'sudo raspiconfig'\n\
+2) Optional: To use the HTTP API and frontend:
+    2.1) Upload SSL certificates to \"$WATCHTOWER_PATH/nginx/certs\". You will need:\n\
+        - A public SSL certificate.
+        - The corresponding private key. These two used for encrypting traffic.
+        - A certificate authority cert for validating clients. Necessary to restrict access to trusted users.
+        These 3 files must match the names defined in \"$WATCHTOWER_PATH/.env\":\n\
+            - 'SSL_CERT=wt.crt'\n\
+            - 'SSL_CERT_KEY=wt.key'\n\
+            - 'SSL_CLIENT_CERT=ca.crt'\n\
+    2.2) Enter the IP address (typically the reverse proxy address) allowed to access this machine by editing 'ALLOWED_CLIENT_IP=x.x.x.x' in \"$WATCHTOWER_PATH/.env\"\n\
+3) Optional: To use a microcontroller, you will need to:\n\
+    3.1) Enable serial access via 'sudo raspiconfig'\n\
+    3.2) Set 'SERIAL_ENABLED=1' in \"$WATCHTOWER_PATH/.env\"\n\
+    3.3) Configure servo angles in \"$WATCHTOWER_PATH/config/watchtower_config.json\"\n\
+4) Optional: Configure the reverse proxy with an upstream location to this machine. See \"$WATCHTOWER_PATH/ancillary/nginx/reverse_proxy\"\n\
+5) Optional: Configure \"$WATCHTOWER_PATH/config/watchtower_config.json\" with Dropbox support."
